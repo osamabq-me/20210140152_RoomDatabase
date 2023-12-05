e31f5a11d7ed3d40efe8f03bb2068f29eecff86b
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-12-05 16:41:18 +0700

    Create the Companion object

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -5,5 +5,7 @@ import com.example.kotlin_repo.repositori.RepositoriSiswa
 
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
-
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }

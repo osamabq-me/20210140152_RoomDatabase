ee3f622a446b54fd6abe04a5bb9e15df7f710ce6
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-12-05 17:01:29 +0700

    Create the Factory and intinizer

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,11 +1,22 @@
 package com.example.roomsiswa.model
 
+import android.text.Spannable.Factory
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.kotlin_repo.model.EntryViewModel
 import com.example.roomsiswa.AplikasiSiswa
 
 object PenyediaViewModel {
-
+val Factory = viewModelFactory {
+    initializer {
+        HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+    }
+    initializer {
+        EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+    }
+}
 }
 
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =

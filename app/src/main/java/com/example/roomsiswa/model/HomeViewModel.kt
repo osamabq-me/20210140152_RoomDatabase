096c16e0ed213769e69714bd32c8dc6af98b8e6b
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-12-05 16:42:27 +0700

    Create the HomeUiState class

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModel
+import com.example.kotlin_repo.data.Siswa
 import com.example.kotlin_repo.repositori.RepositoriSiswa
 
 
@@ -8,4 +9,10 @@ class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+
 }

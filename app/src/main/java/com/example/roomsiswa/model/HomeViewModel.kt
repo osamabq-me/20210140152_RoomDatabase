3cbf234f5c0ad011d37c771afd6987fa0fdbf94d
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-12-05 16:38:44 +0700

    Create the HomeViewModel main class

---
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.roomsiswa.model
+
+import androidx.lifecycle.ViewModel
+import com.example.kotlin_repo.repositori.RepositoriSiswa
+
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+}

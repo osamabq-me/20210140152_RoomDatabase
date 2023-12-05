71b30c4f118cfdab2705342e68db277a090d4c0b
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-12-05 16:13:47 +0700

    Create the rest of the Siswa database querys

--- app/src/main/java/com/example/roomsiswa/data/SiswaDao.kt
+++ app/src/main/java/com/example/roomsiswa/data/SiswaDao.kt
@@ -3,11 +3,27 @@ package com.example.kotlin_repo.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
+    @Query("SELECT * FROM tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
 
 }
